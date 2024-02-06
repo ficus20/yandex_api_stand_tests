a10f1133d92c8cb92c4d7a9c9f238d2f89e323b1
Name <peotr.yakovenko@yandex.ru>
2024-02-06 20:55:20 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://275dc34f-0a11-4b1d-849a-1d1a3b2ee8bd.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+RODUCTS_KITS_PATH = "/api/v1/products/kits/"
